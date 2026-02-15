f32820119f210cbd27ead6a344d20d6a8841ff9a
Alexandre Roux <alex@tekartik.com>
2026-02-15 19:23:33 +0100

    fix lints

--- packages_flutter/playlr_audio_player_just_audio/lib/src/player/source.dart
+++ packages_flutter/playlr_audio_player_just_audio/lib/src/player/source.dart
@@ -2,7 +2,9 @@ import 'dart:typed_data';
 
 import 'package:just_audio/just_audio.dart' as ja;
 
+// ignore: experimental_member_use
 /// A [ja.StreamAudioSource] implementation for playing audio from a [Uint8List] buffer.
+// ignore: experimental_member_use
 class JustAudioBytesSource extends ja.StreamAudioSource {
   /// The audio buffer to play.
   final Uint8List _buffer;
@@ -11,8 +13,10 @@ class JustAudioBytesSource extends ja.StreamAudioSource {
   JustAudioBytesSource(this._buffer) : super(tag: 'JustAudioBytesSource');
 
   @override
+  // ignore: experimental_member_use
   Future<ja.StreamAudioResponse> request([int? start, int? end]) async {
     // Returning the stream audio response with the parameters
+    // ignore: experimental_member_use
     return ja.StreamAudioResponse(
       sourceLength: _buffer.length,
       contentLength: (start ?? 0) - (end ?? _buffer.length),
